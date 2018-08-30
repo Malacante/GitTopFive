4cb5ac90349992fe7f443261758ab0dc5e78d97a
Malacante <malacante@gmail.com>
2018-08-30 14:27:47 -0400

    Bugfix and documents

--- GitTopFive.py
+++ GitTopFive.py
@@ -8,8 +8,8 @@ import time
 if __name__ == "__main__":
 
     try:
-        token = sys.argv[0]
-    except IndexError: #user has not specified their token
+        token = sys.argv[1]
+    except IndexError: # user has not specified their token
         print("Usage: python GitTopFive.py <token>")
         quit()
 
@@ -24,8 +24,11 @@ if __name__ == "__main__":
         if rrep.status_code == 404:
             print("%s is not a valid organization."%org)
             continue
+        if rrep.status_code == 401:
+            print("Your OAuth token is incorrect. Please refer to Readme.txt for instructions on how to generate a token.")
+            quit()
         rep = rrep.json()
-        while True: #handles pagination of results
+        while True:  # handles pagination of results
             try:
                 u = rrep.links["next"]["url"]
                 rrep = requests.get(u)
@@ -37,7 +40,7 @@ if __name__ == "__main__":
                 break
 
         topFive = []
-        for project in tqdm(rep): #iterate through each project and count the pull requests
+        for project in tqdm(rep):  # iterate through each project and count the pull requests
             name = project["name"]
             url = "https://api.github.com/repos/%s/%s/pulls?state=all&access_token=%s&per_page=100" % (org, name, token)
             num = 0
@@ -58,7 +61,7 @@ if __name__ == "__main__":
                     quit()
                 pulls = r.json()
                 num = (pages-1)*100+len(pulls)
-            except KeyError: #only one page
+            except KeyError: # only one page of results
                 pulls = r.json()
                 num = len(pulls)
 
@@ -67,14 +70,14 @@ if __name__ == "__main__":
                 if len(topFive) == 5:
                     topFive.sort(key=operator.itemgetter(1), reverse=True)
                 continue
-            if num <= topFive[-1][1]:
+            if num <= topFive[-1][1]: # if it's smaller than the smallest item we can discard
                 continue
             for x, item in enumerate(topFive):
                 if num > item[1]:
                     topFive.insert(x, (name, num))
                     topFive.pop(-1)
                     break
-        time.sleep(.1) #the progress bar interferes with our results if we try to print them too fast/
+        time.sleep(.1) #the progress bar interferes with our results if we try to print them too fast
         print("Top 5 projects for %s :\n" % org)
         for x, proj in enumerate(topFive):
             print("%s: %d pull request(s)\n" % (proj[0], proj[1]))
