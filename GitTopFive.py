1748ae4f58f554af4ef005195f99cc3af3c547cc
Malacante <malacante@gmail.com>
2018-08-30 00:57:30 -0400

    Pagination for Repos
    Added support for organizations with more than 100 repositories. Added
    proper error handling if user exceeds the API rate limit.

--- GitTopFive.py
+++ GitTopFive.py
@@ -19,17 +19,32 @@ if __name__=="__main__":
         org = input("Enter the organization you wish to query or q to quit.\n")
         if org == 'q':
             quit()
-        rep = requests.get("https://api.github.com/orgs/%s/repos?access_token=65cb1fe8fb137f76629950ba8a692303bfb3b84e&per_page=100" % (org)).json() #get all repos by organization
-        print(rep)
+        if rrep.status_code == 443:
+            print("Rate limit exceeded. Please make sure your token is valid. Otherwise, try again in an hour.")
+            quit()
+        rep = rrep.json()
+        while True:
+            try:
+                u = rrep.links["next"]["url"]
+                rrep = requests.get(u)
+                if rrep.status_code == 443:
+                    print("Rate limit exceeded. Please make sure your token is valid. Otherwise, try again in an hour.")
+                    quit()
+                rep += rrep.json()
+            except KeyError:
+                break
 
         topFive=[]
         for project in rep:
             name = project["name"]
-            owner = project["owner"]["login"]
-            url = "https://api.github.com/repos/%s/%s/pulls?state=all&access_token=65cb1fe8fb137f76629950ba8a692303bfb3b84e&per_page=100" % (owner, name)
+            print("Working on %s" % name)
+            url = "https://api.github.com/repos/%s/%s/pulls?state=all&access_token=65cb1fe8fb137f76629950ba8a692303bfb3b84e&per_page=100" % (org, name)
             num = 0
 
             r = requests.get(url)
+            if r.status_code == 443:
+                print("Rate limit exceeded. Please make sure your token is valid. Otherwise, try again in an hour.")
+                quit()
             try:
                 url = r.links["last"]["url"]
                 p = re.compile('&page=\d+')
@@ -37,6 +52,9 @@ if __name__=="__main__":
                 np = re.compile('\d+')
                 pages = int(np.search(inter).group())
                 r = requests.get(url)
+                if r.status_code == 443:
+                    print("Rate limit exceeded. Please make sure your token is valid. Otherwise, try again in an hour.")
+                    quit()
                 pulls = r.json()
                 num = (pages-1)*100+len(pulls)
             except KeyError: #only one page
@@ -59,16 +77,3 @@ if __name__=="__main__":
         print("Top 5 projects for %s :\n" % org)
         for x, proj in enumerate(topFive):
             print("%s: %d pull request(s)\n" % (proj[0], proj[1]))
-
-
-
-
-
-
-
-
-
-
-
-
-
